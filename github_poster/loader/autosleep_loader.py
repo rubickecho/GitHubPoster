17aba615d98358fb3b7283b02b545210dba64c15
rubickecho <tangliangcheng852@gmail.com>
2023-12-29 09:50:17 +0000

    feat: update autosleep loader, support csv

--- github_poster/loader/autosleep_loader.py
+++ github_poster/loader/autosleep_loader.py
@@ -1,11 +1,12 @@
-import json
+import csv
 from collections import defaultdict
 
+from datetime import datetime
 from github_poster.loader.base_loader import BaseLoader
 
 
 class AutoSleepLoader(BaseLoader):
-    unit = "times"
+    unit = "hour"
 
     def __init__(self, from_year, to_year, _type, **kwargs):
         super().__init__(from_year, to_year, _type)
@@ -18,16 +19,30 @@ class AutoSleepLoader(BaseLoader):
             "--autosleep_file",
             dest="autosleep_file",
             type=str,
-            default="data.json",
+            default="autosleep.csv",
             help="autosleep json file path",
         )
 
+    def _parse_autosleep_data(self):
+        base_file = self.autosleep_file
+        data_obj = {}
+        with open(base_file) as f:
+            reader = csv.DictReader(f)
+            rows = list(reader)
+            # if rows: [{"ISO8601": "2023-01-01T20:59:59+08:00", "睡眠时间": "7:31:00", "效率": "91.4" }, {"ISO8601": "2023-01-02T20:59:59+08:00", "睡眠时间": "6:31:00", "效率": "80" }]
+            # result data_obj: { "2023-01-01": 91.4, "2023-01-02": 80 }
+            for row in rows:
+                time_obj = datetime.strptime(row["睡眠时间"], "%H:%M:%S")
+                hours = time_obj.hour + time_obj.minute / 60 + time_obj.second / 3600
+                date = row["ISO8601"].split("T")[0]
+                data_obj[date] = round(hours, 1)
+        return data_obj
+
     def get_api_data(self):
-        pass
+        return self._parse_autosleep_data()
 
     def make_track_dict(self):
-        with open(self.autosleep_file, "r") as f:
-            tracks = json.load(f)
+        tracks = self.get_api_data()
         self.number_by_date_dict = tracks
         for _, v in self.number_by_date_dict.items():
             self.number_list.append(v)
